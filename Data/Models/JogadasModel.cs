8a3bd73b0eac7c81b8f61ae2584ead14abc3aafa
tulio collenghi <tulio.collenghi@outlook.com>
2021-03-14 12:17:07 -0300

    Atualização de Serviços

---
+++ Data/Models/JogadasModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Repository.Models
+{
+    [Serializable]
+    public class JogadasModel
+    {
+        public JogadasModel()
+        { }
+
+        public JogadasModel(int id, string jogada)
+        {
+            this.id = id;
+            this.Jogada = jogada;
+        }
+
+        #region Instance Properties
+
+        [Display(Name = "id")]
+        [Required]
+        public Int32 id { get; set; }
+
+        [Display(Name = "Jogada")]
+        [Required, StringLength(100)]
+        public String Jogada { get; set; }
+
+        #endregion Instance Properties
+    }
+}
---
+++ Services/JogadasServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using Repository.Models;
+
+namespace Services
+{
+    public class JogadasServices
+    {
+        public static List<JogadasModel> GetJogadas()
+        {
+            var listaJogadas = new List<JogadasModel>();
+            SetJogadas(listaJogadas);
+            return listaJogadas;
+        }
+
+        private static void SetJogadas(List<JogadasModel> listaJogadas)
+        {
+            listaJogadas.Add(new JogadasModel(1, "Pedra"));
+            listaJogadas.Add(new JogadasModel(2, "Tesoura"));
+            listaJogadas.Add(new JogadasModel(3, "Papel"));
+        }
+    }
+}
