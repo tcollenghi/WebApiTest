a0d3d852fd52fe65d2c04d2383dea414516393bc
tulio collenghi <tulio.collenghi@outlook.com>
2021-03-14 18:39:28 -0300

    SignalR

---
+++ Presentation/Hubs/MessageHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Presentation.Hubs
+{
+    public class MessageHub : Hub
+    {
+        // Broadcast para todos os jogadores
+        public async Task Send(string name, string message)
+        {
+            await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+    }
+}
--- Presentation/Startup.cs
+++ Presentation/Startup.cs
@@ -24,6 +24,7 @@ namespace Presentation
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
